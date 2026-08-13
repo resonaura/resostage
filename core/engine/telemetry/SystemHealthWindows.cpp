6899712d04a1346521a1b9f5e9ec5527f52a1fa4
Andrii Vynohradov <andrii.vynohradov@b4value.net>
2026-08-13 03:17:39 -0700

    Windows: define NOMINMAX to avoid std::max macro clash

--- core/engine/telemetry/SystemHealthWindows.cpp
+++ core/engine/telemetry/SystemHealthWindows.cpp
@@ -20,6 +20,9 @@
 //  - Disk I/O counters come from GetProcessIoCounters.
 
 #ifdef _WIN32
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
